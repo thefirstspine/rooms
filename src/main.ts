fd5fdc2e12cb96e600ba96c825349005a63b2b6c
thefirstspine <teddy@coretizone.com>
2020-06-02 20:49:27 +0200

    Removed used of env shared assets prior to dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LogService } from './@shared/log-shared/log.service';
 import { ErrorFilter } from './error.filter';
-import env from './@shared/env-shared/env';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Load dotenv config
+  require('dotenv').config();
+
+  // Start app
+  const app = await NestFactory.create(AppModule.register());
   app.useGlobalFilters(new ErrorFilter(new LogService('rooms')));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(env.dist ? env.config.PORT : 2505);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
